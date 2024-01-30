d7443132421f26e4022e70a99bd053068139deda
dimitar.dimitrov <dimitar.dimitrov@sirma.bg>
2024-01-30 18:28:24 +0200

    initial + read file to console

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.Buffer;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+//        readFile("./input.txt");
+
+
+    }
+
+    private static void readFile(String path) {
+        StringBuilder result = new StringBuilder();
+        try (FileInputStream in = new FileInputStream(path)) {
+            int theByte = in.read();
+
+            while (theByte != -1) {
+                result.append(theByte).append(", ");
+                theByte = in.read();
+            }
+
+            System.out.println(result);
+
+        } catch (IOException err) {
+            System.out.println(err.getMessage());
+            err.printStackTrace();
+        }
+    }
+
+}
