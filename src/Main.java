f97b1f407ca92f78df752bf8f6f20fff97928e83
dimitar.dimitrov <dimitar.dimitrov@sirma.bg>
2024-01-30 18:48:52 +0200

    write to file

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.Buffer;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -8,7 +12,33 @@ public class Main {
     public static void main(String[] args) {
 
 //        readFile("./input.txt");
+        writeToFile("./input.txt");
 
+    }
+
+    private static void writeToFile(String path) {
+        List<Character> sybols = new ArrayList<>(Arrays.asList(',', '.', '!', '?', ';'));
+
+        try (FileInputStream in = new FileInputStream(path);
+             FileOutputStream out = new FileOutputStream("./result.txt")
+        ) {
+            int symbol = in.read();
+
+            while (symbol != -1) {
+                if (sybols.contains((char) symbol)) {
+                    symbol = in.read();
+
+                    continue;
+                }
+
+                out.write(symbol);
+                symbol = in.read();
+            }
+
+        } catch (IOException err) {
+            System.out.println(err.getMessage());
+            err.printStackTrace();
+        }
 
     }
 
@@ -29,5 +59,4 @@ public class Main {
             err.printStackTrace();
         }
     }
-
 }
